fd21c12cf31be2a02cff745b885187fa99f1c542
CLG\dmoore <devante.moore@cambiumassessment.com>
2021-12-03 13:11:43 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <router-view />
+    <div class="bottom_bar">
+      <div class="row expanded">
+        <div class="sm-4 columns text-center">
+          <router-link to="/">
+            <i class="fa fa-home" aria-hidden="true"></i>
+            <span>Home</span>
+          </router-link>
+        </div>
+        <div class="sm-4 columns text-center">
+          <router-link to="/add_task">
+            <i class="fa fa-plus" aria-hidden="true" />
+            <span>Add Tasks</span>
+          </router-link>
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
 
-#nav {
-  padding: 30px;
 }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style lang="scss" src="./assets/scss/app.scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,25 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from '@/views/Home/Home.vue';
+import AddTask from '@/views/AddTask/AddTask.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    path: "/add_task",
+    name: "AddTask",
+    component: AddTask
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
+
+console.log("history", process.env.BASE_URL);
 
 export default router;
